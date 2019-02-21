adba80226159ab98d6a1fcb71ace9cc9647b4da6
tony <tony.li@vechain.com>
2019-02-21 22:50:13 +0800

    add typed and print css

--- src/reveal.config.ts
+++ src/reveal.config.ts
@@ -1,19 +1,31 @@
-import Reveal from 'reveal.js'
 import hljs from 'highlight.js'
 import javascript from 'highlight.js/lib/languages/javascript'
 import '@/reveal-plugins/highlight'
+import { initTypedJs } from '@/reveal-plugins/typed'
 
+// CSS
 import 'reveal.js/css/reveal.css'
 import 'reveal.js/css/theme/black.css'
 // Theme used for syntax highlighting of code
 import 'reveal.js/lib/css/zenburn.css'
-import 'reveal.js/lib/js/head.min'
 
-Reveal.initialize({})
-// reveal plugins
+// Printing and PDF exports
+if (window.location.search.match(/print-pdf/gi)) {
+    require('reveal.js/css/print/pdf.css')
+} else {
+    require('reveal.js/css/print/paper.css')
+}
+
+// Reveal plugins
 import 'imports-loader?Reveal=reveal.js/js/reveal!reveal.js/plugin/markdown/marked'
 import 'imports-loader?Reveal=reveal.js/js/reveal!reveal.js/plugin/markdown/markdown'
 import 'imports-loader?Reveal=reveal.js/js/reveal!reveal.js/plugin/notes/notes'
 
+import Reveal from 'reveal.js'
+Reveal.initialize({
+    history: true,
+})
+
 hljs.initHighlightingOnLoad()
 hljs.registerLanguage('javascript', javascript)
+initTypedJs()
